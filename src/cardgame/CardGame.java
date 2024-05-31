55a15308af947fc47196efabfacdef525bfbf718
tanishvyas009 <vyastan@sheridancollege.ca>
2024-05-30 22:54:00 -0400

    update ICE1

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -1,20 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package cardgame;
+
+import java.util.Random;
 
 /**
  *
- * @author fagun
+ * @author Tanish 991736234
  */
 public class CardGame {
+
+    private String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    private String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    private String[] hand;
+
+    public CardGame() {
+        this.hand = new String[7];
+        this.dealHand();
+    }
+
+    private void dealHand() {
+        Random random = new Random();
+        for (int i = 0; i < 7; i++) {
+            String value = values[random.nextInt(values.length)];
+            String suit = suits[random.nextInt(suits.length)];
+            this.hand[i] = value + " of " + suit;
+        }
+    }
+
+    public void printHand() {
+        for (String card : this.hand) {
+            System.out.println(card);
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CardGame game = new CardGame();
+        game.printHand();
     }
-    
 }
